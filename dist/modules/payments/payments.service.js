9ff026200ff67f714725f99562077df094d78735
Adewale <adeosunadewale86@gmail.com>
2026-02-08 21:30:21 +0100

    REF

--- dist/modules/payments/payments.service.js
+++ dist/modules/payments/payments.service.js
@@ -203,6 +203,7 @@ let PaymentsService = class PaymentsService {
                 data: { amount: subtotal / 100, updatedAt: new Date() },
             });
         }
+        //.X
         const customerUrl = this.config.get('CUSTOMER_URL') || this.config.get('FRONTEND_URL') || 'http://localhost:5173';
         const successUrl = `${customerUrl}/success?order_id=${dto.order_id}`;
         const cancelUrl = `${customerUrl}/cancel?order_id=${dto.order_id}`;
